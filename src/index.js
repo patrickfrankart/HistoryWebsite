6fb5da5725960375de173b62236253af2cb0edee
patrickfrankart <patrick.frankart@gmail.com>
2022-05-03 10:45:59 -0400

    Adjusted image arrays according to notes from last meeting. Fixed problem with initial category image not loading in slideshow.

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function Image(url, alt, caption){
 }
 
 function setDisplay(images){
-  var photo = document.getElementById("photo");
+    var photo = document.getElementById("photo");
 	var temp = images[1];
 	photo.src = temp.url;
 	photo.alt = temp.alt;
@@ -35,16 +35,18 @@ function setDisplay(images){
 function advanceImage(back){
     if(back){
         index = (index-1)%images.length;
-        if(index === 0){
+        if(index === -1){
             index = images.length-1;
         }
     }
     else{
         index = (index+1)%images.length;
     }
+    /*
     if(index === 0){
         index = 1;
     }
+    */
     var photo = document.getElementById('photo');
     var replace = images[index];
     photo.src = replace.url;
